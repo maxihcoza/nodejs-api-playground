28067df0fc2d2c6328841c1d810660ccae5b21a0
Maxine Harris <discovermaxine@gmail.com>
2021-02-02 16:07:17 +0200

    initial working app

---
+++ server.js
@@ -0,0 +1,12 @@
+//create a simple HTTP server on my local machine
+
+//load the HTTP library
+////var http = require("http");
+
+//create an HTTP server to handle responses
+/*
+http.createServer(function(request, response) {
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write("Hello World");
+    response.end();
+}).listen(8888);*/
